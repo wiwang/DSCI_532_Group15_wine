f79edb32301906d5e9f664084b31647cc41d3970
Zhiyong Wang <wiwang@student.ubc.ca>
2021-01-19 18:36:08 -0800

    plot the layout

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import altair as alt
 import pandas as pd
@@ -8,57 +9,58 @@ alt.data_transformers.disable_max_rows()
 
 
 # Read in data
-#wine_df = pd.read_csv("C:\Users\lihua\MDS\4_532_data_visualization-2\DSCI_532_Group15_wine\data\processed\wine.csv")
-wine_df = pd.read_csv("../data/processed/wine.csv")
-print(wine_df)
+wine_df = pd.read_csv("./data/processed/wine.csv")
 
 countries = wine_df["country"].dropna().unique()
 country_list = list(countries)
 
-
 # Setup app and layout/frontend
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = dbc.Container([
-    html.H1('My mudplank'),
+    html.H1('Wine Valley'),
+
+    #The first row
     dbc.Row([
+        #add search criteria
         dbc.Col([
             dcc.Dropdown(
-                id='xcol-widget',
-                value='Horsepower',  # REQUIRED to show the plot on the first page load
-                options=[{'label': col, 'value': col} for col in cars.columns]),
-            dcc.Dropdown(
-                id='ycol-widget',
-                value='Displacement',  # REQUIRED to show the plot on the first page load
-                options=[{'label': col, 'value': col} for col in cars.columns])],
-            md=4),
+                id='country-widget',
+                options=[{'label': country, 'value': country} for country in country_list],
+                value='US')
+        ], md=4),
+        #add search results
         dbc.Col([
-            dbc.Row([
-                dbc.Col(
-                    dbc.Card(
-                        dbc.CardBody(html.H5('Key value')),
-                        color='warning')),
-                dbc.Col(
-                    dbc.Card(
-                        dbc.CardBody(html.H5('Key value')),
-                        color='info', inverse=True, style={'text-align': 'center'}))]),
+            html.Iframe(
+                id='search-result',
+                srcDoc=alt.Chart(wine_df).mark_bar().encode(
+                    x = 'country',
+                    y = 'price'
+                ).to_html()
+            )
+        ])
+    ]),
+
+    #The second row
+    dbc.Row([
+        #add geometry map
+        dbc.Col([
+            html.Iframe(
+                id='map-result',
+                srcDoc=alt.Chart(wine_df).mark_bar().encode(
+                    x = 'country',
+                    y = 'price'
+                ).to_html()
+            )
+        ], md=4),
+        #add statistic results
+        dbc.Col([
             html.Iframe(
                 id='scatter',
-                style={'border-width': '0', 'width': '100vw', 'height': '100vh'})])])])
-
-# Setup app and layout/frontend
-# app = dash.Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
-app.layout = html.Div([
-        html.Iframe(
-            id='scatter',
-            #srcDoc=plot_altair(),
-            style={'border-width': '0', 'width': '100%', 'height': '400px'}),
-        dcc.Dropdown(
-            id='country-widget',
-            options=[{'label': country, 'value': country} for country in country_list],
-            value='US')  ])
-
-
-
+                #srcDoc=plot_altair(),
+                style={'border-width': '0', 'width': '100%', 'height': '400px'})            
+        ])
+    ])
+])
 
 # Set up callbacks/backend
 @app.callback(
